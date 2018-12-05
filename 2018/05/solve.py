20d18558c503a9763cbcffb0aee79ece6f92f5de
Joscha <brummer@migejolise.de>
2018-12-05 16:27:42 +0000

    Attempt to clean up 2018/05

--- 2018/05/solve.py
+++ 2018/05/solve.py
@@ -15,20 +15,34 @@ def naive_react(pol):
 			print("Naive react says: More reacting is possible.")
 
 def react(pol):
-	start = 0
 	while True:
-		i = start
+		i = 0
 		while i < len(pol) - 1:
-			if pol[i].lower() == pol[i+1].lower() and pol[i] != pol[i+1]:
+			pol_cur, pol_next = pol[i], pol[i+1]
+			if pol_cur.lower() == pol_next.lower() and pol_cur != pol_next:
 				del pol[i]
 				del pol[i]
-				start = max(0, start - 1)
-				break
-			elif i == start + 1:
-				start = i
-			i += 1
+				i = max(0, i - 1)
+			else:
+				i += 1
 		else:
 			return
+
+#def react(pol):
+#	start = 0
+#	while True:
+#		i = start
+#		while i < len(pol) - 1:
+#			if pol[i].lower() == pol[i+1].lower() and pol[i] != pol[i+1]:
+#				del pol[i]
+#				del pol[i]
+#				start = max(0, start - 1)
+#				break
+#			elif i == start + 1:
+#				start = i
+#			i += 1
+#		else:
+#			return
 
 def result(pol):
 	l = pol.copy()
