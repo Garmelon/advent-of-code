583cf9322adfd16a3b66cf817553ed81daa8defa
Joscha <joscha@plugh.de>
2022-12-01 17:24:45 +0100

    [rs] Set up very basic framework for running days

---
+++ rs/src/d01.rs
@@ -0,0 +1,4 @@
+pub fn solve(input: String) -> anyhow::Result<()> {
+    println!("TODO!");
+    Ok(())
+}
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod d01;
+
+use std::fs;
+use std::path::PathBuf;
+
+use clap::{Parser, ValueEnum};
+
+#[derive(Clone, Copy, PartialEq, Eq, ValueEnum)]
+enum Day {
+    D01,
+}
+
+#[derive(Parser)]
+struct Args {
+    day: Day,
+    file: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let input = fs::read_to_string(&args.file)?;
+
+    match args.day {
+        Day::D01 => d01::solve(input)?,
+    }
+
+    Ok(())
 }
