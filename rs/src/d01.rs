5c17b5a556ab0b816759930bb2c398ce31c27fa8
Joscha <joscha@plugh.de>
2022-12-01 18:18:46 +0100

    [rs] Simplify 2022_01

--- rs/src/d01.rs
+++ rs/src/d01.rs
@@ -1,22 +1,14 @@
 pub fn solve(input: String) -> anyhow::Result<()> {
-    let mut elves = vec![];
-    let mut elf = vec![];
-    for line in input.lines() {
-        if let Ok(number) = line.trim().parse::<u32>() {
-            elf.push(number);
-        } else {
-            elves.push(elf);
-            elf = vec![];
-        }
-    }
-    if !elf.is_empty() {
-        elves.push(elf);
-    }
-
-    let mut elves = elves
-        .into_iter()
-        .map(|e| e.into_iter().sum())
-        .collect::<Vec<u32>>();
+    let mut elves = input
+        .trim()
+        .split("\n\n")
+        .map(|chunk| {
+            chunk
+                .split('\n')
+                .map(|n| n.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<_>>();
     elves.sort_unstable();
 
     // Part 1
