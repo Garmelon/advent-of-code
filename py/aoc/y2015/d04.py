55a9a39d9dc0d84cc0601d6e1a594be5b3bee37d
Joscha <joscha@plugh.de>
2022-12-06 21:25:40 +0100

    [py] Simplify 2015_04

--- py/aoc/y2015/d04.py
+++ py/aoc/y2015/d04.py
@@ -1,29 +1,17 @@
 import hashlib
 
-# PART 1
 
-
-def leading_zeroes(amount, start):
+def brute_force(seed, start):
     n = 1
-    what = "0" * amount
     while True:
-        text = (start + str(n)).encode("utf-8")
+        text = f"{seed}{n}".encode("utf-8")
         h = hashlib.md5(text).hexdigest()
-        if h[:amount] == what:
+        if h.startswith(start):
             return n
-
-        # if n % 100000 == 0:
-        #     print(f"{n:9} {text} {h}")
-
         n += 1
 
 
-# PART 2
-
-
 def solve(inputstr):
-    hashstart = inputstr.strip()
-    n = leading_zeroes(5, hashstart)
-    print(f"Part 1: {n}")
-    n_2 = leading_zeroes(6, hashstart)
-    print(f"Part 2: {n_2}")
+    seed = inputstr.strip()
+    print(f"Part 1: {brute_force(seed, '00000')}")
+    print(f"Part 2: {brute_force(seed, '000000')}")
