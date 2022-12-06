a2a69a373a3ec90856e94fb5cf39b8a2157ffdda
Joscha <joscha@plugh.de>
2022-12-06 09:50:12 +0100

    [rs] Solve 2022_06 part 2

--- rs/src/y2022/d06.rs
+++ rs/src/y2022/d06.rs
@@ -1,17 +1,25 @@
+use core::panic;
+use std::collections::{HashSet, VecDeque};
+
+fn scan(input: &str, lookback: usize) -> usize {
+    let mut last_n = VecDeque::with_capacity(lookback);
+    for (i, c) in input.chars().enumerate() {
+        last_n.push_back(c);
+        while last_n.len() > lookback {
+            last_n.pop_front();
+        }
+        if last_n.len() < lookback {
+            continue;
+        }
+        let last_n_set = last_n.iter().copied().collect::<HashSet<_>>();
+        if last_n_set.len() == lookback {
+            return i;
+        }
+    }
+    panic!()
+}
+
 pub fn solve(input: String) {
-    let part1 = input
-        .chars()
-        .zip(input.chars().skip(1))
-        .zip(input.chars().skip(2))
-        .zip(input.char_indices().skip(3))
-        .find_map(|(((c1, c2), c3), (i, c4))| {
-            if c1 != c2 && c1 != c3 && c1 != c4 && c2 != c3 && c2 != c4 && c3 != c4 {
-                println!("{c1}{c2}{c3}{c4}");
-                Some(i + 1)
-            } else {
-                None
-            }
-        })
-        .unwrap();
-    println!("Part 1: {part1}");
+    println!("Part 1: {}", scan(&input, 4) + 1);
+    println!("Part 2: {}", scan(&input, 14) + 1);
 }
