455aa617ba69cd2cd94580efd7bc6e7b858cd583
Joscha <joscha@plugh.de>
2022-12-06 21:03:25 +0100

    [py] Simplify 2015_02

--- py/aoc/y2015/d02.py
+++ py/aoc/y2015/d02.py
@@ -1,45 +1,16 @@
-import re
-
-PACKET_RE = r"(\d+)x(\d+)x(\d+)"
-
-
-def load_packets(inputstr):
-    packets = []
-    for line in inputstr.splitlines():
-        match = re.fullmatch(PACKET_RE, line)
-        a, b, c = match.groups()
-        a, b, c = int(a), int(b), int(c)
-        packets.append((a, b, c))
-    return packets
+def area(l, w, h):
+    sides = [l * w, w * h, h * l]
+    return 2 * sum(sides) + min(sides)
 
 
-# PART 1
-
-
-def necessary_area(packet):
-    a, b, c = sorted(packet)
-    return 3 * a * b + 2 * a * c + 2 * b * c
-
-
-def total_wrapping_paper(packets):
-    return sum(map(necessary_area, packets))
-
-
-# PART 2
-
-
-def ribbon_length(packet):
-    a, b, c = sorted(packet)
-    return 2 * a + 2 * b + a * b * c
-
-
-def total_ribbon_length(packets):
-    return sum(map(ribbon_length, packets))
+def ribbon(l, w, h):
+    half_perimeters = [l + w, w + h, h + l]
+    return 2 * min(half_perimeters) + l * w * h
 
 
 def solve(inputstr):
-    packets = load_packets(inputstr)
-    total = total_wrapping_paper(packets)
-    print(f"Part 1: {total}")
-    total_2 = total_ribbon_length(packets)
-    print(f"Part 2: {total_2}")
+    boxes = [tuple(map(int, line.split("x"))) for line in inputstr.splitlines()]
+    part1 = sum(area(l, w, h) for l, w, h in boxes)
+    print(f"Part 1: {part1}")
+    part2 = sum(ribbon(l, w, h) for l, w, h in boxes)
+    print(f"Part 2: {part2}")
